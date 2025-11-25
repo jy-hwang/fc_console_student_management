68c5111af279e11949bbedb9cb95772d00265ca9
jy-hwang <stratojy@gmail.com>
2025-11-25 19:05:51 +0900

    feat. 일급 컬렉션 이용해보기
    1. 1급 컬렉션은 컬렉션을 래핑하여 단 하나의 멤버 변수만을 가지는 객체를 의미함.
    2. 이러한 구조는 컬렉션에 대한 조작을 통제하고 비즈니스 로직을 캡슐화하는데 유용하기 때문.
    3. 수강료 계산 부분을 따로 빼서 1급 컬렉션으로 만들어보기

--- src/main/java/org/fastcampus/student_management/application/course/CourseService.java
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -3,6 +3,7 @@ package org.fastcampus.student_management.application.course;
 import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
 import org.fastcampus.student_management.domain.Course;
+import org.fastcampus.student_management.domain.CourseList;
 import org.fastcampus.student_management.domain.DayOfWeek;
 import org.fastcampus.student_management.domain.Student;
 import org.fastcampus.student_management.repo.CourseRepository;
@@ -43,14 +44,7 @@ public class CourseService {
     // TODO: 과제 구현 부분
     List<Course> courses = courseRepository.getCourseListByStudent(studentName);
 
-    for (Course course : courses) {
-      // 수강일이 주말(토요일 또는 일요일)인 경우 수강료는 1.5배
-      if (course.isSameDay(DayOfWeek.SATURDAY) || course.isSameDay(DayOfWeek.SUNDAY)) {
-        course.changeFee((int) (fee * 1.5));
-      }
-
-      course.changeFee(fee);
-    }
-
+    CourseList courseList = new CourseList(courses);
+    courseList.changeAllCoursesFee(fee);
   }
 }
